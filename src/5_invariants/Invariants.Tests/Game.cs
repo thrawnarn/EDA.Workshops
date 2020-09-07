da29e941f1f1c3dc26e8b8383803085081847763
Per Ökvist <perokvist@users.noreply.github.com>
2020-09-07 13:03:39 +0200

    exercise 5

--- src/5_invariants/Invariants.Tests/Game.cs
+++ src/5_invariants/Invariants.Tests/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Invariants.Tests
@@ -8,14 +9,30 @@ namespace Invariants.Tests
     {
         public static IEnumerable<IEvent> Handle(JoinGame command, GameState state)
         {
-            yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
-            yield return new RoundStarted { GameId = command.GameId, Round = 1 };
+            if (state.Players.PlayerOne.Id == command.PlayerId)
+                yield break;
+
+            if (state.Players.PlayerTwo == default)
+            {
+                yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
+                yield return new RoundStarted { GameId = command.GameId, Round = 1 };
+            }
         }
 
         public static IEnumerable<IEvent> Handle(JoinGame command, IEvent[] events)
         {
-            yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
-            yield return new RoundStarted { GameId = command.GameId, Round = 1 };
+            if (events
+                .OfType<GameCreated>()
+                .Any(e => e.PlayerId == command.PlayerId))
+                yield break;
+
+            if (!events
+                .OfType<GameStarted>()
+                .Any(e => e.PlayerId == command.PlayerId))
+            {
+                yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
+                yield return new RoundStarted { GameId = command.GameId, Round = 1 };
+            }
         }
 
     }
--- src/5_invariants/Invariants.Tests/GameState.cs
+++ src/5_invariants/Invariants.Tests/GameState.cs
@@ -1,8 +1,29 @@
-﻿namespace Invariants.Tests
+﻿using System;
+
+namespace Invariants.Tests
 {
     public class GameState
     {
+        public Guid Id { get; set; }
+        public (Player PlayerOne, Player PlayerTwo) Players { get; set; }
+        public int Round { get; set; }
+        public int Rounds { get; set; }
+
         public GameState When(IEvent @event) => this;
+
+        public GameState When(GameCreated @event)
+        {
+            Id = @event.GameId;
+            Players = (new Player { Id = @event.PlayerId }, default);
+            Rounds = @event.Rounds;
+            return this;
+        }
+        public class Player
+        {
+            public string Id { get; set; }
+            public Hand Hand { get; set; }
+        }
+
     }
 
 }
