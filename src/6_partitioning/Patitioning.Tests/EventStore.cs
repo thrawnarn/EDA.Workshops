ca4b42f9b02bdb2bac84e8cf8119cbb873555d84
Per Ökvist <perokvist@users.noreply.github.com>
2020-10-12 09:39:43 +0200

    exercise 6 fix readme

--- src/6_partitioning/Patitioning.Tests/EventStore.cs
+++ src/6_partitioning/Patitioning.Tests/EventStore.cs
@@ -57,20 +57,4 @@ namespace Patitioning.Tests
         public static void Append(this EventStore store, params IEvent[] events)
             => events.ToList().ForEach(e => store.Append(e.GetType().Name, e));
     }
-
-    public static class EventStoreRoomExtensions
-    { 
-            public static string[] GetCheckedInRoomIds(this EventStore store)
-            => store.Read(0, int.MaxValue)
-                .Select(x => x.Content)
-				.OfType<IRoomEvent>()
-				.GroupBy(o => o.RoomId)
-				.Where(o => o.Last() is RoomCheckedIn)
-				.Select(o => o.Key)
-				.ToArray();
-
-        public static string[] GetRoomsToClean(this EventStore store)
-            => Array.Empty<string>();
-
-    }
 }
