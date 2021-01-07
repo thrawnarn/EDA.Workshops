d85b5e370fbe5189cb150672032a3db66c2bb926
Per Ökvist <perokvist@users.noreply.github.com>
2021-01-07 13:22:45 +0100

    game ended

--- src/ts/1_rps/RPS/game.test.ts
+++ src/ts/1_rps/RPS/game.test.ts
@@ -15,13 +15,13 @@ test('started', () => {
     //given
     const created: game.GameCreated = { gameId: 'test', sourceId: 'test', playerId: '', title: '', rounds: 1, created: new Date() }
     const gameStarted: game.GameStarted = { gameId: 'test', sourceId: 'test', playerId: '' }
-    const roundStarted: game.RoundStarted = { gameId: 'test', sourceId: 'test', round : 1}
+    const roundStarted: game.RoundStarted = { gameId: 'test', sourceId: 'test', round: 1 }
 
     const events: game.GameEvent[] =
         [
             { type: 'gameCreated', event: created },
             { type: 'gameStarted', event: gameStarted },
-            { type: 'roundStarted', event: roundStarted}
+            { type: 'roundStarted', event: roundStarted }
         ]
     const state = { status: game.GameStatus.None }
 
@@ -30,4 +30,34 @@ test('started', () => {
 
     //then
     expect(newState.status).toBe(game.GameStatus.Started);
+});
+
+test('ended', () => {
+
+    //given
+    const created: game.GameCreated = { gameId: 'test', sourceId: 'test', playerId: '', title: '', rounds: 1, created: new Date() }
+    const gameStarted: game.GameStarted = { gameId: 'test', sourceId: 'test', playerId: '' }
+    const roundStarted: game.RoundStarted = { gameId: 'test', sourceId: 'test', round: 1 }
+    const handShownLisa: game.HandShown = { gameId: 'test', sourceId: 'test', hand: game.Hand.Paper, playerId: "lisa@tester.com" }
+    const handShownAlex: game.HandShown = { gameId: 'test', sourceId: 'test', hand: game.Hand.Rock, playerId: "alex@tester.com" }
+    const roundEnded: game.RoundEnded = { gameId: 'test', sourceId: 'test', round: 1, looser: "lisa@tester.com", winner: "alex@tester.com" }
+    const gameEnded: game.GameEnded = { gameId: 'test', sourceId: 'test' }
+
+    const events: game.GameEvent[] =
+        [
+            { type: 'gameCreated', event: created },
+            { type: 'gameStarted', event: gameStarted },
+            { type: 'roundStarted', event: roundStarted },
+            { type: 'handShown', event: handShownLisa },
+            { type: 'handShown', event: handShownAlex },
+            { type: 'roundEnded', event: roundEnded },
+            { type: 'gameEnded', event: gameEnded }
+        ]
+    const state = { status: game.GameStatus.None }
+
+    //when
+    const newState = events.reduce(game.apply, state)
+
+    //then
+    expect(newState.status).toBe(game.GameStatus.Ended);
 });
--- src/ts/1_rps/RPS/game.ts
+++ src/ts/1_rps/RPS/game.ts
@@ -1,7 +1,20 @@
 type GameCreated = { readonly gameId: string; readonly playerId: string; readonly title: string; readonly rounds: number; readonly created: Date; sourceId: string }
-type GameStarted = { readonly gameId: string; playerId: string; sourceId: string }
-type RoundStarted = { readonly gameId: string; round: number; sourceId: string }
-type GameEvent = { type: "gameCreated"; event: GameCreated } | { type: "gameStarted"; event: GameStarted } | { type: "roundStarted"; event: RoundStarted }
+type GameStarted = { readonly gameId: string; readonly playerId: string; readonly sourceId: string }
+type RoundStarted = { readonly gameId: string; readonly round: number; readonly sourceId: string }
+type HandShown = { readonly gameId: string; readonly hand: Hand; readonly playerId: string; readonly sourceId: string }
+type RoundEnded = { readonly gameId: string; readonly round: number; readonly looser: string; readonly winner: string; readonly sourceId: string }
+type GameEnded = { readonly gameId: string; readonly sourceId: string }
+
+enum Hand { None = 0, Rock = 10, Paper = 20, Scissors = 30 }
+
+type GameEvent =
+    { type: "gameCreated"; event: GameCreated } |
+    { type: "gameStarted"; event: GameStarted } |
+    { type: "roundStarted"; event: RoundStarted } |
+    { type: "handShown"; event: HandShown } |
+    { type: "roundEnded"; event: RoundEnded } |
+    { type: "gameEnded"; event: GameEnded }
+
 type GameState = { status: GameStatus }
 
 enum GameStatus { None = 0, ReadyToStart = 10, Started = 20, Ended = 50 }
@@ -12,10 +25,12 @@ function apply(state: GameState, e: GameEvent): GameState {
             return { ...state, status: GameStatus.ReadyToStart }
         case 'gameStarted':
             return { ...state, status: GameStatus.Started }
+        case 'gameEnded':
+            return { ...state, status: GameStatus.Ended }
         default:
             return state;
     }
 }
 
-export { apply, GameCreated, GameStarted, RoundStarted, GameEvent, GameStatus };
+export { apply, GameCreated, GameStarted, RoundStarted, HandShown, RoundEnded, GameEnded, GameEvent, GameStatus, Hand };
 
