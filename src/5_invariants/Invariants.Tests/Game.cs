9d2242b341bd000231cce5e5fb2b62905346126d
Anders Liljekvist <thrawn@jungfrubo.se>
2021-02-01 09:20:29 +0100

    Finish day one

--- src/5_invariants/Invariants.Tests/Game.cs
+++ src/5_invariants/Invariants.Tests/Game.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Invariants.Tests
@@ -8,12 +9,20 @@ namespace Invariants.Tests
     {
         public static IEnumerable<IEvent> Handle(JoinGame command, GameState state)
         {
+            if(state.Players.Any(p=>p.Id.Equals(command.PlayerId)))
+                yield break;
+
             yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
             yield return new RoundStarted { GameId = command.GameId, Round = 1 };
         }
 
         public static IEnumerable<IEvent> Handle(JoinGame command, IEvent[] events)
         {
+            if (events.OfType<GameCreated>().Any(p => p.PlayerId.Equals(command.PlayerId)))
+                yield break;
+            if (!events.OfType<GameCreated>().Any())
+                yield break;
+
             yield return new GameStarted { GameId = command.GameId, PlayerId = command.PlayerId };
             yield return new RoundStarted { GameId = command.GameId, Round = 1 };
         }
