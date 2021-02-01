59f47feefb35f7a4f4f4a7c1a05fa6c71c10ff24
Anders Liljekvist <thrawn@jungfrubo.se>
2021-02-01 11:22:14 +0100

    Finished #7

--- src/7_eventstore/EventStore.Tests/EventStore.cs
+++ src/7_eventstore/EventStore.Tests/EventStore.cs
@@ -32,10 +32,10 @@ namespace Subscription.Tests
         {
             var lastVersion = currentValue.Any() ? currentValue.Last().EventVersion : 0;
 
-            if (false) //TODO fix
+            if (concurreny.check && lastVersion > 0 && lastVersion + 1 != concurreny.version)
                 throw new DBConcurrencyException($"wrong version - expected {concurreny.version} but was {lastVersion} - in stream {streamName}");
 
-            var duplicates = Array.Empty<EventData>(); //TODO fix
+            var duplicates = currentValue.Where(p => events.Any(a => a.EventId.Equals(p.EventId))).ToList();
             if (duplicates.Any())
                 throw new Exception($"Tried to append duplicates in stream - {streamName}. {string.Join(',', duplicates.Select(d => $"{d.EventName} - {d.EventId}"))}");
 
